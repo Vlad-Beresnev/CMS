5974df8be46d183287645fa65088d97332a2dfe9
Vlad-Beresnev <beresnev.explorer@gmai.com>
2023-11-16 12:00:28 +0200

    add user, del user, prevent to del youself

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
-const { TIMEOUT } = require('dns');
 const fs = require('fs')
+const urlModule = require('url')
 
 const registerUser = (userData) => {
     const database = JSON.parse(fs.readFileSync('database.json', 'utf-8'));
@@ -15,6 +15,19 @@ const authenticateUser = (username, password) => {
     console.log('Authenticated User:', user);
     return user
 }
+
+const deleteUser = (usernameToDelete) => {
+    const database = JSON.parse(fs.readFileSync('database.json', 'utf-8'));
+    const indexToDelete = database.users.findIndex(user => user.username === usernameToDelete);
+
+    if (indexToDelete !== -1) {
+        database.users.splice(indexToDelete, 1);
+        fs.writeFileSync('database.json', JSON.stringify(database, null, 2));
+        return true; // User deleted successfully
+    }
+
+    return false; // User not found
+};
 
 const existingUser = (username) => {
     const database = JSON.parse(fs.readFileSync('database.json', 'utf-8'))
@@ -70,9 +83,10 @@ const authRoutes = (req, res) => {
                     userSession["password"] = user.password
                     userSession["role"] = user.role
                 
-                    res.writeHead(200, { 'Contente-Type': 'text/plain'})
-                    res.end('Login Successful')
+                    res.writeHead(200, { 'Contente-Type': 'application/json'})
+                    res.end(JSON.stringify(userSession), null, 2)
                     console.log(userSession)
+                    console.log(userSession.username)
                 } else {
                     res.writeHead(401, { 'Contente-Type': 'text/plain'})
                     res.end('Invalid credentials')
@@ -100,43 +114,78 @@ const authRoutes = (req, res) => {
         } 
         // POST METHOD
         else if (req.method === 'POST') {
-            let data = '';
-            req.on('data', (chunk) => {
-                data += chunk
-            })
+            if (userSession["role"] === 'admin') {
+                let data = '';
+                req.on('data', (chunk) => {
+                    data += chunk
+                })
 
-            req.on('end', () => {
-                try {
-                const { username, password, role } = JSON.parse(data)
+                req.on('end', () => {
+                    try {
+                    const { username, password, role } = JSON.parse(data)
 
-                if (!username || !password || !role) {
-                    res.writeHead(400, { 'Content-Type': 'text/plain' })
-                    res.end('Username, password and role required')
-                } else {
-                    const userExist = existingUser(username)
-
-                    if (userExist) {
-                        res.writeHead(409, { 'Content-Type': 'text/plain' })
-                        res.end('User already exists')
+                    if (!username || !password || !role) {
+                        res.writeHead(400, { 'Content-Type': 'text/plain' })
+                        res.end('Username, password and role required')
                     } else {
-                        registerUser({ username, password, role })
-                        console.log('User registered:', { username, password })
-                        res.writeHead(201, { 'Content-Type': 'text/plain' })
-                        res.end('User registered successfully')
-                        }
+                        const userExist = existingUser(username)
+
+                        if (userExist) {
+                            res.writeHead(409, { 'Content-Type': 'text/plain' })
+                            res.end('User already exists')
+                        } else {
+                            registerUser({ username, password, role })
+                            console.log('User created:', { username, password, role })
+                            res.writeHead(201, { 'Content-Type': 'text/plain' })
+                            res.end('User created successfully')
+                            }
                     }
                 } catch (error) {
                     console.error('Error parsing user data:', error);
                     res.writeHead(500, { 'Content-Type': 'text/plain' });
                     res.end('Internal Server Error');
                 }
-            })
-        // DELETE METHOD
-        } else if (req.method === 'DELETE') {
-        } else {
-            res.writeHead(405, { 'Content-Type': 'text/plain', 'Allow': 'GET, POST' });
-            res.end('Method Not Allowed');
-        }
+                })
+            } else {
+                res.writeHead(403, { 'Content-Type': 'text/plain' });
+                res.end('Forbidden: Only admin users can access this route');
+            }
+            
+        } 
+    // DELETE METHOD
+    } else if (req.method === 'DELETE') {
+            if (userSession.role === 'admin') {
+                const database = JSON.parse(fs.readFileSync('database.json', 'utf-8'));
+                const parsedUrl = urlModule.parse(req.url, true);
+                const userId = parsedUrl.pathname.split('/').pop(); // Extract the user id from the URL
+                if (!userId) {
+                    res.writeHead(400, { 'Content-Type': 'text/plain' });
+                    res.end('User ID to delete is required');
+                } else {
+                    const userIndex = database.users.findIndex(user => user.username === userId);
+            
+                    if (userIndex === -1) {
+                        res.writeHead(404, { 'Content-Type': 'text/plain' });
+                        res.end('User not found');
+                    } else if (userId === userSession.username) {
+                        res.writeHead(400, { 'Content-Type': 'text/plain' });
+                        res.end('You can not delete yourself!');
+                    } else {
+                        // Remove the user from the array
+                        const deletedUser = database.users.splice(userIndex, 1)[0];
+                        
+                        // Save the updated database to the file
+                        fs.writeFileSync('database.json', JSON.stringify(database, null, 2));
+                        console.log(`User ${userId} deleted successfully!`)
+                        res.writeHead(200, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify(deletedUser, null, 2));
+                    }
+                }
+            } else {
+                res.writeHead(403, { 'Content-Type': 'text/plain' });
+                res.end('Forbidden: Only admin users can access this route');
+            }
+                
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
