ad6a83d301114cfb6a86f27f38843d6227f025ee
Vlad-Beresnev <beresnev.explorer@gmai.com>
2023-11-27 16:32:17 +0200

    logout page, view 1, navbar is in the process

--- public/js/home.js
+++ public/js/home.js
@@ -2,14 +2,16 @@ function toggleSidebar() {
     const sidebar = document.getElementById('sidebar')
     const menuIcon = document.querySelector('.menu-icon i')
 
-    sidebar.classList.toggle('open')
-    sidebar.classList.toggle('close')
-
-
-    if (sidebar.classList.contains('open')) {
-        menuIcon.classList.remove('fa-bars')
-        menuIcon.classList.add('fa-x') 
+    if (!sidebar.classList.contains('open')) {
+        // If sidebar is not open, open it
+        sidebar.classList.remove('close');
+        sidebar.classList.add('open');
+        menuIcon.classList.remove('fa-bars');
+        menuIcon.classList.add('fa-x');
     } else {
+        // If sidebar is open, close it
+        sidebar.classList.remove('open');
+        sidebar.classList.add('close');
         menuIcon.classList.remove('fa-x');
         menuIcon.classList.add('fa-bars');
     }
