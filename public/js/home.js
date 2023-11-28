a9e5c1ddf081c31cd12d19b3753a29774a9c4ebb
Vlad-Beresnev <beresnev.explorer@gmai.com>
2023-11-28 19:45:00 +0200

    logoutpage view 1, editing is in the process

--- public/js/home.js
+++ public/js/home.js
@@ -16,3 +16,74 @@ function toggleSidebar() {
         menuIcon.classList.add('fa-bars');
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const greetingTextElement = document.querySelector('.view1 h1');
+    const greetEditIcon = document.getElementById('greetEdit');
+    const greetingInput = document.getElementById('greetingInput');
+
+    let isEditing = false;
+ 
+
+    const editGreeting = () => {
+        const newGreetingText = greetingInput.value;
+
+        fetch('/home', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ newGreetingText }),
+        })
+            .then((res) => res.text())
+            .then((message) => {
+                alert(message);
+                greetingTextElement.style = originalGreetingTextStyle;
+                greetEditIcon.style = originalGreetEditIconStyle;
+                greetingTextElement.textContent = newGreetingText;
+                greetingTextElement.classList.remove('editing');
+                greetingTextElement.style.display = 'inline';
+                greetingInput.style.display = 'none';
+                greetEditIcon.style.marginTop = '-22px'
+                
+                greetingTextElement.addEventListener('mouseover', () => {
+                    greetEditIcon.style.display = 'inline';
+                });
+                greetEditIcon.addEventListener('mouseover', () => {
+                    greetEditIcon.style.display = 'inline';
+                });
+            
+                greetingTextElement.addEventListener('mouseout', () => {
+                    greetEditIcon.style.display = 'none';
+                });
+    
+
+                isEditing = false;
+            })
+            .catch((error) => console.error(error));
+    };
+
+    const toggleEdit = () => {
+        if (isEditing) {
+            editGreeting();
+        } else {
+
+            greetingTextElement.classList.add('editing');
+            greetingTextElement.style.display = 'none';
+            greetingInput.style.display = 'inline';
+            greetEditIcon.style.display = 'inline';
+            greetEditIcon.style.marginTop = '336px';
+            greetingInput.style.marginTop = '-60px';
+        
+            isEditing = true;
+        }
+    };
+
+    greetEditIcon.addEventListener('click', toggleEdit);
+
+    document.addEventListener('click', (event) => {
+        if (isEditing && event.target !== greetEditIcon && event.target !== greetingInput) {
+            editGreeting();
+        }
+    });
+});
