db2111d3a8ea0ee24660cd3d07104b3acc495b4c
Vlad-Beresnev <beresnev.explorer@gmail.com>
2023-12-06 15:57:51 +0200

    mobile log out page fullstack is done

--- public/js/home.js
+++ public/js/home.js
@@ -16,11 +16,91 @@ function toggleSidebar() {
         menuIcon.classList.add('fa-bars');
     }
 }
-const greetingElement = document.getElementById('greetingText')
+
 document.addEventListener('DOMContentLoaded', () => {
+    const greetingElement = document.getElementById('greetingText')
     const database = JSON.parse(fs.readFileSync('database.json', 'utf-8'))
     const logOutPage = database.pages.find((page) => page.id === '1')
     const greetingText = logOutPage.page_heading
     greetingElement.textContent = greetingText
 
 })
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const greetingElement = document.getElementById('greetingText');
+    const greetEdit = document.getElementById('greetEdit');
+    const greetFinish = document.getElementById('greetFinish');
+    const greetingInput = document.getElementById('greetingInput');
+
+
+    // Function to update greeting text on the page
+    const updateGreetingText = (newGreetingText) => {
+        greetingElement.textContent = newGreetingText;
+    };
+
+    // Fetch the initial greeting text from the server
+    fetch('/database.json')
+        .then(response => response.json())
+        .then(data => {
+            const logOutPage = data.pages.find((page) => page['id'] === '1');
+            if (logOutPage) {
+                updateGreetingText(logOutPage.page_heading);
+            }
+        })
+        .catch(error => console.error(error));
+
+
+    greetEdit.addEventListener('click', () => {
+        greetingInput.style.display = 'inline'
+        greetFinish.style.display = 'inline'
+        
+        // Send a PUT request to update the greeting text
+    greetFinish.addEventListener('click', () => {
+        const newGreetingText = greetingInput.value;
+        
+        fetch('/home', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ page_heading: newGreetingText }),
+        })
+        .then(response => response.json())
+        .then(updatedPage => {
+            updateGreetingText(updatedPage.page_heading);
+            // You can perform additional actions or show messages here if needed
+        })
+        .catch(error => console.error(error));
+        location.reload();
+    })
+    });
+
+    document.addEventListener('click', (event) => {
+        const isClickInsideEditIcon = greetEdit.contains(event.target);
+        const isClickInsideFinishIcon = greetFinish.contains(event.target);
+        const isClickInsideGreetingInput = greetingInput.contains(event.target) 
+
+        if (!isClickInsideEditIcon && !isClickInsideFinishIcon && !isClickInsideGreetingInput && greetFinish.style.display === 'inline') {
+            greetingInput.style.display = 'none';
+            greetFinish.style.display = 'none';
+            const newGreetingText = greetingInput.value;
+        
+            fetch('/home', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ page_heading: newGreetingText }),
+            })
+            .then(response => response.json())
+            .then(updatedPage => {
+                updateGreetingText(updatedPage.page_heading);
+                // You can perform additional actions or show messages here if needed
+            })
+            .catch(error => console.error(error));
+            location.reload();
+            }
+    })
+
+});
